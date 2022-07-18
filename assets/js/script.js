a021bd86e403289e256a44d3e502c14b52579882
assofiejakobsson <s89j@live.se>
2022-07-18 13:06:26 +0000

    Add varibel and addEventListiner to the choise an game player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("hej");
+const computerChoiseDisplay = document.getElementById("computer-choise");
+const yourChoiseDisplay = document.getElementById("your-choise");
+const resultDisplay = document.getElementById("result");
+const allGameChoise = document.querySelectorAll("button");
+let yourChoise
+
+allGameChoise.forEach(allGameChoise => allGameChoise.addEventListener('click', (e) => {
+    yourChoise = e.target.id
+    yourChoiseDisplay.innerHTML = yourChoise
+}))
